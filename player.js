983a840ca5f3844b55df2fa6fd9b5d655dcd0294
backupepsi <72424100+backupepsi@users.noreply.github.com>
2020-12-29 14:21:07 -0500

    Add files via upload

---
+++ player.js
@@ -0,0 +1,63 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+    getScore1(){
+        var score =database.ref('Score/player1');
+        score.on("value", (data)=>{
+        ps1 = data.val();
+        })
+    }
+    getScore2(){
+        var score =database.ref( 'Score/player2');
+        score.on("value" ,(data)=>{
+        ps2 = data.val();
+        })
+    }
+    updateS1(vari){
+        database.ref('Score/player1').update({
+            'Score/player1' : vari
+        })
+    }
+    updateS2(vari){
+        database.ref('Score/player2')({
+            'Score/player2' : vari
+        })
+    }
+
+
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
